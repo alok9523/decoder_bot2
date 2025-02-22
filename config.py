0a6e5727d358ba64aa47cb31a314d49ae5fe1eec
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-02-22 13:12:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
 API_KEY = "ddc-0D0phWHsoLGsNgnEJ0U1HTm2W0wZF01hkI1wKBS3ih8KK7EENC"
 BOT_TOKEN = "6044696246:AAFbr9ewoPfuGf6RCFYq66HlVaYBH5x_tkM"
+OWNER_NAME = "Alok Kumar"  # Replace with the owner's name
+OWNER_ID = 6195379665 # Replace with the owner's Telegram ID
+
+ADMIN_IDS = [OWNER_ID]  # Add more admin IDs if needed
